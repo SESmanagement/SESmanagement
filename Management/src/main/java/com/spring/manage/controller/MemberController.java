dc9edf77f74c31f82cc7bfd7f7ffa1775eb9f1e0
SCITMASTER <SCITMASTER@LAPTOP-CKF6AO2S>
2017-08-22 10:26:08 +0900

    아아아아 세션저장안되는 상태 - ㄹㄷ

--- Management/src/main/java/com/spring/manage/controller/MemberController.java
+++ Management/src/main/java/com/spring/manage/controller/MemberController.java
@@ -4,8 +4,10 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.manage.service.MemberService;
 import com.spring.manage.vo.MemberVO;
@@ -24,14 +26,22 @@ public class MemberController {
 		vo.setPwd(pwd);
 		vo = service.login(vo);
 		session.setAttribute("vo", vo);
-		return "home";
+		System.out.println("포스트인데왜뜸");
+		return "redirect:/";
 	}
 	
+	@RequestMapping(value = "logout", method = RequestMethod.POST)
+	public String logout(HttpSession session) {
+		System.out.println("오잉");
+		session.invalidate();
+		System.out.println(session.getAttribute("name"));
+		return "redirect:/";
+		
+	}
 	
 	@RequestMapping(value = "join", method = RequestMethod.GET)
 	public String join() {
-		return "join";
-		
+		return "join";	
 	}
 	
 	
