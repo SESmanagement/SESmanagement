585258e54f4bf7973d13738700894c2476e8f5d2
rokdam <30626388+rokdam@users.noreply.github.com>
2017-09-22 20:39:39 +0900

    그룹, 멤버서비스 추가 0922 20:39 록담

--- Management/src/main/java/com/spring/manage/service/GroupService.java
+++ Management/src/main/java/com/spring/manage/service/GroupService.java
@@ -1,15 +1,27 @@
 package com.spring.manage.service;
 
 import java.util.ArrayList;
+import java.util.Map;
 
+import com.spring.manage.util.PageNavigator;
 import com.spring.manage.vo.GroupJoinVO;
 import com.spring.manage.vo.GroupVO;
 
 public interface GroupService {
 	
 	public boolean seek(GroupVO vo);
-	public ArrayList<GroupVO> list();
-	public ArrayList<GroupVO> detail(int group_num);
+	public ArrayList<GroupVO> list(Map<String, String> map, PageNavigator navi);
 	public boolean join(GroupJoinVO vo2);
-
+	public PageNavigator getNavi(int currentPage, Map<String, String> map);
+	public int member_count(int group_num);
+	public ArrayList<GroupVO> mygroup(GroupJoinVO vo);
+	public ArrayList<GroupVO> team(GroupJoinVO vo);
+	public ArrayList<GroupJoinVO> include();
+	public ArrayList<GroupVO> daredoko(GroupJoinVO gjvo);
+	public GroupVO detail(GroupJoinVO gjvo);
+	public int penalty(GroupJoinVO gjvo);
+	public ArrayList<GroupVO> grouppen();
+	public int insert_penalty(GroupJoinVO gjvo);
+	public ArrayList<GroupJoinVO> penalty_list(GroupJoinVO gjvo);
+	public int modify_penalty(GroupJoinVO gjvo);
 }
--- Management/src/main/java/com/spring/manage/service/GroupServiceImpl.java
+++ Management/src/main/java/com/spring/manage/service/GroupServiceImpl.java
@@ -1,11 +1,13 @@
 package com.spring.manage.service;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.manage.dao.GroupDAOImpl;
+import com.spring.manage.util.PageNavigator;
 import com.spring.manage.vo.GroupJoinVO;
 import com.spring.manage.vo.GroupVO;
 
@@ -14,32 +16,100 @@ public class GroupServiceImpl implements GroupService {
 	
 	@Autowired
 	private GroupDAOImpl dao;
+	private final int countPerPage = 9;
+	private final int pagePerGroup = 10;
 	
 	@Override
 	public boolean seek(GroupVO vo) {
-		if(dao.seek(vo)<1){	//insert가 업데이트 되지 못해 결과가 0
+		System.out.println("서비스"+vo);
+		if( dao.seek(vo) > 0){	//insert가 업데이트 되서 결과가 1 이상
+			GroupJoinVO vo2 = new GroupJoinVO(vo.getGroup_num(), vo.getStudent_num(), 0, null, null);
+			if(dao.join(vo2)>0) return true;
+			else return false;
+		}else{				//insert 업데이트 못되서 결과가 0
 			return false;
-		}else{				//insert 업데이트 되서 결과가 1 이상
-			return true;
 		}
 	}
 	
 	@Override
-	public ArrayList<GroupVO> list(){
-		return dao.list();
+	public ArrayList<GroupVO> list(Map<String, String> map, PageNavigator navi){
+		return dao.list(map, navi.getStartRecord(), navi.getCountPerPage());
 	}
 
+	
 	@Override
-	public ArrayList<GroupVO> detail(int group_num) {
-		System.out.println("서비스"+dao.detail(group_num));
-		return dao.detail(group_num);
+	public int member_count(int group_num){
+		return dao.member_count(group_num);
+	}
+	
+	
+	@Override
+	public PageNavigator getNavi(int currentPage, Map<String, String> map) {
+		int totalRecordsCount = dao.total_count(map);
+		PageNavigator navi = new PageNavigator(countPerPage, pagePerGroup, currentPage, totalRecordsCount);
+		return navi;
+	}
+
+	
+	@Override
+	public ArrayList<GroupVO> daredoko(GroupJoinVO gjvo){
+		return dao.daredoko(gjvo);
+	}
+	
+	@Override
+	public GroupVO detail(GroupJoinVO gjvo) {
+		return dao.detail(gjvo);
 	}
 
 	@Override
 	public boolean join(GroupJoinVO vo2) {
-		if(dao.join(vo2)>0) return true;
+		if(dao.join(vo2)>0){
+			return true;
+		}
 		else return false;
-	
 	}
+
+	@Override
+	public ArrayList<GroupVO> mygroup(GroupJoinVO vo){
+		return dao.mygroup(vo);
+	}
+
+	@Override
+	public ArrayList<GroupVO> team(GroupJoinVO vo){
+		return dao.team(vo);
+	}
+
+	@Override
+	public ArrayList<GroupJoinVO> include() {
+		return dao.include();
+	}
+
+	@Override
+	public int penalty(GroupJoinVO gjvo) {
+		return dao.penalty(gjvo);
+	}
+	
+	@Override
+	public ArrayList<GroupVO> grouppen(){
+		return dao.grouppen();
+		}
+
+	@Override
+	public int insert_penalty(GroupJoinVO gjvo) {
+		return dao.insert_penalty(gjvo);
+	}
+
+	
+	@Override
+	public int modify_penalty(GroupJoinVO gjvo) {
+		return dao.modify_penalty(gjvo);
+	}
+
+	@Override
+	public ArrayList<GroupJoinVO> penalty_list(GroupJoinVO gjvo) {
+		return dao.penalty_list(gjvo);
+	}
+
+
 
 }
