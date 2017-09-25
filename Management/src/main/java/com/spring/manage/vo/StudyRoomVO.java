885cff83ae853d22b78355cdf11d7c148944288e
ulala8725 <30626463+ulala8725@users.noreply.github.com>
2017-09-25 22:54:57 +0900

    Update StudyRoomVO.java

--- Management/src/main/java/com/spring/manage/vo/StudyRoomVO.java
+++ Management/src/main/java/com/spring/manage/vo/StudyRoomVO.java
@@ -3,7 +3,6 @@ package com.spring.manage.vo;
 public class StudyRoomVO { //
 	private int studyroom_num; // 스터디룸 번호
 	private int sr_seat_num; // 각 스터디룸 내 좌석 번호
-	private char rechargable; // 충전가능여부 (좌석 근처에 콘세트 유무 여부를 Y와 N으로 표시)
 	private String seated_status; // 4개의 시간대중 현재 예약된 시간대의 수
 	private int room_x; // 스터디룸의 위치 좌표
 	private int room_y; // 스터디룸의 위치 좌표
@@ -33,15 +32,7 @@ public class StudyRoomVO { //
 	public void setSr_seat_num(int sr_seat_num) {
 		this.sr_seat_num = sr_seat_num;
 	}
-
-	public char getRechargable() {
-		return rechargable;
-	}
-
-	public void setRechargable(char rechargable) {
-		this.rechargable = rechargable;
-	}
-
+	
 	public String getSeated_status() {
 		return seated_status;
 	}
@@ -132,8 +123,8 @@ public class StudyRoomVO { //
 
 	@Override
 	public String toString() {
-		return "StudyRoomVO [studyroom_num=" + studyroom_num + ", sr_seat_num=" + sr_seat_num + ", rechargable="
-				+ rechargable + ", seated_status=" + seated_status + ", room_x=" + room_x + ", room_y=" + room_y
+		return "StudyRoomVO [studyroom_num=" + studyroom_num + ", sr_seat_num=" + sr_seat_num + ", + ", seated_status=" 
+				+ seated_status + ", room_x=" + room_x + ", room_y=" + room_y
 				+ ", seat_x=" + seat_x + ", seat_y=" + seat_y + ", activate=" + activate + ", seven=" + seven
 				+ ", eight=" + eight + ", nine=" + nine + ", ten=" + ten + ", count_seat=" + count_seat + "]";
 	}
