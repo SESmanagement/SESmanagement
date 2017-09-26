c875120fbf38fe6ba070ac8d0d0b2428cf6976a6
seonghyeon000 <seonghyeon000@gmail.com>
2017-09-26 13:33:40 +0900

    Update books.sql

--- Management/src/main/resources/sql/books.sql
+++ Management/src/main/resources/sql/books.sql
@@ -1,29 +1,28 @@
 -- books 시퀀스 생성
-CREATE SEQUENCE BOOK_SEQ;
-
--- books table 생성
-
-CREATE TABLE books
-(
-	num number PRIMARY KEY,
-	title varchar2(100) not ull,
-	publisher varchar2(100) not null,
-	author varchar2(100) not null,
-	content varchar2(100) not null,
-	imageurl varchar2(100) not null
-);		
-
+CREATE SEQUENCE BOOK_SEQ;	
 
 --lend 시퀀스
 CREATE sequence lend_seq
 start with 1
 increment by 1;
 
---lend table생성
+
+----book table
+CREATE TABLE books
+(
+	num number PRIMARY KEY,
+	title varchar2(100) not null,
+	publisher varchar2(100) not null,
+	author varchar2(100) not null,
+	content varchar2(100) not null,
+	imageurl varchar2(100) not null
+);		
+
+---- lend table
 CREATE TABLE lend
 (
 	num number PRIMARY KEY,
-	mem_num varchar2(50) references member(num),
+	mem_num number references member(student_num),
 	booknum number references books(num),
 	startdate date not null,
 	enddate date,
