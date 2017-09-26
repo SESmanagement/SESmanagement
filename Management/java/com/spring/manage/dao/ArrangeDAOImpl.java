d24c8cc3530914750432b509307da09344707fed
eureka37 <february37@naver.com>
2017-09-26 10:24:56 +0900

    Update ArrangeDAOImpl.java

--- Management/java/com/spring/manage/dao/ArrangeDAOImpl.java
+++ Management/java/com/spring/manage/dao/ArrangeDAOImpl.java
@@ -9,7 +9,11 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.spring.manage.vo.ArrangeVO;
 import com.spring.manage.vo.ClassRoomVO;
+import com.spring.manage.vo.Exam_ScoreVO;
+import com.spring.manage.vo.MemberVO;
+import com.spring.manage.vo.Member_NoteVO;
 import com.spring.manage.vo.PartyVO;
 import com.spring.manage.vo.Room_StatusVO;
 
@@ -57,21 +61,69 @@ public class ArrangeDAOImpl implements ArrangeDAO{
 		
 	}
 
-	//강의실에 좌석 입력
-	//@Override
-	/*public void insertSeat(List<Room_StatusVO> status) {
+	@Override
+	public ArrayList<Room_StatusVO> getRoom_Status(int num) {
 		ArrangeMapper mapper = sqlSession.getMapper(ArrangeMapper.class);
-		Map<String, List<Room_StatusVO>> statusList = new HashMap<>(); 
-		statusList.put("list", status);
-		for(Room_StatusVO vo : status){
-			System.out.println(vo);
-		}
-		mapper.insertSeat(statusList);
-		for(int i = 0 ; i < statusList.size(); i++){
-			System.out.println(statusList.get(i));
-			mapper.insertSeat(statusList.get(i));
-		}
-	}*/
+		return mapper.getRoom_Status(num);
+	}
+
+	@Override
+	public ClassRoomVO selectRoom(int classRoom_num) {
+		ArrangeMapper mapper = sqlSession.getMapper(ArrangeMapper.class);
+		return mapper.selectRoom(classRoom_num);
+	}
+
+	@Override
+	public int changeServiceAble(Room_StatusVO status) {
+		ArrangeMapper mapper = sqlSession.getMapper(ArrangeMapper.class);
+		System.out.println("여기까지 옴? 여긴 : DAO");
+		return mapper.changeServiceAble(status);
+		
+	}
+
+	@Override
+	public ArrayList<ClassRoomVO> callRooms(int num) {
+		ArrangeMapper mapper = sqlSession.getMapper(ArrangeMapper.class);
+		return mapper.callRooms(num);
+	}
+
+	@Override
+	public int countPartyMember(int party_num) {
+		ArrangeMapper mapper = sqlSession.getMapper(ArrangeMapper.class);
+		return mapper.countPartyMember(party_num);
+	}
+
+	@Override
+	public ArrayList<MemberVO> callMember(int party_num) {
+		ArrangeMapper mapper = sqlSession.getMapper(ArrangeMapper.class);
+		return mapper.callMember(party_num);
+	}
+
+	@Override
+	public ArrayList<Exam_ScoreVO> callScore() {
+		ArrangeMapper mapper = sqlSession.getMapper(ArrangeMapper.class);
+		return mapper.callScore();
+	}
+
+	@Override
+	public ArrayList<Member_NoteVO> callNote() {
+		ArrangeMapper mapper = sqlSession.getMapper(ArrangeMapper.class);
+		return mapper.callNote();
+	}
+
+	@Override
+	public int countService(int classroom_num) {
+		ArrangeMapper mapper = sqlSession.getMapper(ArrangeMapper.class);
+		return mapper.countService(classroom_num);
+	}
+
+	@Override
+	public void arrangeClass(ArrangeVO member) {
+		ArrangeMapper mapper = sqlSession.getMapper(ArrangeMapper.class);
+		mapper.arrangeClass(member);
+	}
+
+	
 
 	
 }
