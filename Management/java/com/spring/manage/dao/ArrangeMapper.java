a2b7b802566b6a19f7e5e8302128d76c4c4b3936
eureka37 <february37@naver.com>
2017-09-26 10:25:56 +0900

    Update ArrangeMapper.java

--- Management/java/com/spring/manage/dao/ArrangeMapper.java
+++ Management/java/com/spring/manage/dao/ArrangeMapper.java
@@ -1,10 +1,12 @@
 package com.spring.manage.dao;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
+import com.spring.manage.vo.ArrangeVO;
 import com.spring.manage.vo.ClassRoomVO;
+import com.spring.manage.vo.Exam_ScoreVO;
+import com.spring.manage.vo.MemberVO;
+import com.spring.manage.vo.Member_NoteVO;
 import com.spring.manage.vo.PartyVO;
 import com.spring.manage.vo.Room_StatusVO;
 
@@ -13,6 +15,16 @@ public interface ArrangeMapper {
 	public ArrayList<PartyVO> getPartyList();
 	public void updateRoom(ClassRoomVO room);
 	public int deleteSeat(ClassRoomVO room);
-	//public void insertSeat(Map<String, List<Room_StatusVO>> statusList);
 	public void insertSeat(Room_StatusVO status);
+	public ArrayList<Room_StatusVO> getRoom_Status(int classRoom_num);
+	public ClassRoomVO selectRoom(int num);
+	public int changeServiceAble(Room_StatusVO status); 
+	public ArrayList<ClassRoomVO> callRooms(int num);
+	public int countPartyMember(int party_num);
+	public ArrayList<MemberVO> callMember(int party_num);
+	public ArrayList<Exam_ScoreVO> callScore();
+	public ArrayList<Member_NoteVO> callNote();
+	public int countService(int classroom_num);
+	public void arrangeClass(ArrangeVO member);
+
 }
