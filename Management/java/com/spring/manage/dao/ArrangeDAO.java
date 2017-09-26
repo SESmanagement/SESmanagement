050423b8acd46206de38797494c5572b715009d1
eureka37 <february37@naver.com>
2017-09-26 10:24:36 +0900

    Update ArrangeDAO.java

--- Management/java/com/spring/manage/dao/ArrangeDAO.java
+++ Management/java/com/spring/manage/dao/ArrangeDAO.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.spring.manage.vo.ArrangeVO;
 import com.spring.manage.vo.ClassRoomVO;
+import com.spring.manage.vo.Exam_ScoreVO;
+import com.spring.manage.vo.MemberVO;
+import com.spring.manage.vo.Member_NoteVO;
 import com.spring.manage.vo.PartyVO;
 import com.spring.manage.vo.Room_StatusVO;
 
@@ -14,6 +18,16 @@ public interface ArrangeDAO {
 	public ArrayList<PartyVO> getPartyList();
 	public void updateRoom(ClassRoomVO room);
 	public int deleteSeat(ClassRoomVO room);
-//	public void insertSeat(List<Room_StatusVO> status);
 	public void insertSeat(Room_StatusVO status);
+	public ArrayList<Room_StatusVO> getRoom_Status(int classRoom_num);
+	public ClassRoomVO selectRoom(int num);
+	public int changeServiceAble(Room_StatusVO status); 
+	public ArrayList<ClassRoomVO> callRooms(int num);
+	public int countPartyMember(int party_num);
+	public ArrayList<MemberVO> callMember(int party_num);
+	public ArrayList<Exam_ScoreVO> callScore();
+	public ArrayList<Member_NoteVO> callNote();
+	public int countService(int classroom_num);
+	public void arrangeClass(ArrangeVO arrList);
+	
 }
