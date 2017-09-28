5d9cc6b2b4a38a571e86772a3c788d6f24452a4e
ulala8725 <30626463+ulala8725@users.noreply.github.com>
2017-09-28 18:23:24 +0900

    Update StudyRoomController.java

--- Management/java/com/spring/manage/controller/StudyRoomController.java
+++ Management/java/com/spring/manage/controller/StudyRoomController.java
@@ -39,7 +39,6 @@ public class StudyRoomController {//
 	@RequestMapping(value = "getStudyRoomInfo", method = RequestMethod.POST)
 	@ResponseBody
 	public ArrayList<StudyRoomVO> getStudyRoomInfo(int studyroom_num) {
-		System.out.println("스터디룸: " + studyroom_num);
 		service.updateSeated_status();
 		return service.showInside(studyroom_num);
 	}
@@ -55,10 +54,6 @@ public class StudyRoomController {//
 	@RequestMapping(value = "getSRFacilityInfo", method = RequestMethod.POST)
 	@ResponseBody
 	public ArrayList<SR_FacilityVO> getSRFacilityInfo(int studyroom_num) {
-		System.out.println("부대시설: " + studyroom_num);
-		for (SR_FacilityVO vo : service.getSRFacilityInfo(studyroom_num)) {
-			System.out.println("부대시설: " + vo);
-		}
 		return service.getSRFacilityInfo(studyroom_num);
 	}
 	
@@ -79,7 +74,6 @@ public class StudyRoomController {//
 	// 관리자 메뉴- 스터디룸의 좌석 / 부대시설 삭제하기
 	@RequestMapping(value = "dropSR_Seat", method = RequestMethod.POST) // 
 	public String dropSR_Seat(String selectedList, Model model) {
-		System.out.println("컨트롤러 dropSR_Seat");
 		String result = service.dropSR_Seat(selectedList);
 		return "redirect:adminPage?result="+result;
 	}
@@ -121,9 +115,6 @@ public class StudyRoomController {//
 			@RequestParam(value="etc_checkList", defaultValue="") ArrayList<String> etc_checkList,
 			@RequestParam(value="currentPage", defaultValue="1") int currentPage) {
 		
-//		// 페이징
-//		PageNavigator navi = service.getNavi(currentPage);
-		
 		// 검색 조건 model에 담기
 		model.addAttribute("studyroom_checkList", studyroom_checkList);
 		model.addAttribute("date_checkList", date_checkList);
@@ -133,14 +124,6 @@ public class StudyRoomController {//
 		returnMap = service.getResvList(studyroom_checkList, date_checkList, etc_checkList, currentPage);
 		resvList = (ArrayList<SR_ReservationVO>) returnMap.get("resvList");
 		navi = (PageNavigator) returnMap.get("navi");
-		System.out.println("TotalPageCount: " + navi.getTotalPageCount());
-//		System.out.println("TotalRecordsCount: " + navi.getTotalRecordsCount());
-		System.out.println("====CountPerPage: " + navi.getCountPerPage());
-		System.out.println("====StartRecord: " + navi.getStartRecord());
-		System.out.println("CurrentPage: " + navi.getCurrentPage());
-		System.out.println("PagePerGroup: " + navi.getPagePerGroup());
-		System.out.println("StartPageGroup: " + navi.getStartPageGroup());
-		System.out.println("EndPageGroup: " + navi.getEndPageGroup());
 		
 		// 검색할 때 저 3개를 안 가지고 가서 전체를 끌고 옴... 그래서 한번에 다 펼쳐짐????
 		model.addAttribute("resvList", resvList);
@@ -154,7 +137,6 @@ public class StudyRoomController {//
 	// top메뉴 ->스터디룸 현황조회(top-스터디룸 예약-메뉴 포함)로 이동
 	@RequestMapping(value = "rooms", method = RequestMethod.GET)
 	public String rooms(Model model, ArrayList<SR_ReservationVO> allSrList) {
-		System.out.println("컨트롤러 rooms");
 		model.addAttribute("sysdate", service.getSysdate());
 		return "studyroom/rooms";
 	}
@@ -178,7 +160,7 @@ public class StudyRoomController {//
 		model.addAttribute("sysdate", sysdate);
 		return "studyroom/reserveRoomForm";
 	}
-	//만날 수 없어 말하고 싶은데 그런 슬픈 기분인걸 말하고 싶어 말하고 싶은데 속마음만 숨기는걸 내 사랑의 마지막 열쇠가 있다면 그건 바로 이 세상이 아름다운 이유 캐치유 캐치유 캐치미 캐치미 이제 숨바꼭질은 그만 우울한거 모두 파란하늘에 묻어버려
+	
 	// 해당 스터디룸의 예약현황 
 	@RequestMapping(value = "srResvInfo", method = RequestMethod.POST)
 	@ResponseBody
@@ -193,10 +175,6 @@ public class StudyRoomController {//
 		MemberVO vo = (MemberVO) session.getAttribute("vo");
 		int student_num = vo.getStudent_num(); // 세션의 학생번호
 		service.resvApply(resvApplyArr, student_num); // 예약 신청 하기
-//		myResvList = service.myResvInfo(student_num); // 내 예약 현황 가져오기
-//		sysdate = service.getSysdate(); //  오늘 날짜 + 현재 시간 가져오기
-//		model.addAttribute("myResvList", myResvList);
-//		model.addAttribute("sysdate", sysdate);
 		model.addAttribute("student_num", student_num);
 		return "redirect:myResvInfoPage?student_num="+student_num;
 	}
@@ -205,7 +183,6 @@ public class StudyRoomController {//
 	@RequestMapping(value = "myResvInfo")
 	@ResponseBody
 	public ArrayList<SR_ReservationVO> myResvInfo(int student_num, ArrayList<SR_ReservationVO> myResvList, Model model) {
-		System.out.println("컨트롤러 myResvInfo1");
 		myResvList = service.myResvInfo(student_num);
 		return myResvList;
 	}
@@ -214,7 +191,6 @@ public class StudyRoomController {//
 	@RequestMapping(value = "myResvInfoPage")
 	public String myResvInfoPage(int student_num, ArrayList<SR_ReservationVO> myResvList, Model model, String sysdate,
 								String cancelResult) {
-		System.out.println("컨트롤러 myResvInfo2");
 		myResvList = service.myResvInfoAll(student_num);
 		sysdate = service.getSysdate();
 		model.addAttribute("myResvList", myResvList);
@@ -238,11 +214,6 @@ public class StudyRoomController {//
 	public ArrayList<StudyRoomVO> checkAvailableRoom(String checkList, ArrayList<StudyRoomVO> availableRoomList,
 											@RequestParam(value="checkList[]") ArrayList<Integer> checkListList) {
 		availableRoomList = service.checkAvailableRoom(checkListList);
-		
-		for (StudyRoomVO vo : availableRoomList) {
-			System.out.println("check 조회가능리스트: " + vo);
-		}
-		
 		return availableRoomList;
 	}
 	
@@ -250,13 +221,7 @@ public class StudyRoomController {//
 	@RequestMapping(value = "nowAvailableRoom", method = RequestMethod.GET)
 	@ResponseBody
 	public ArrayList<StudyRoomVO> nowAvailableRoom(ArrayList<StudyRoomVO> nowAvailableRoomList, String now_time) {
-		System.out.println("현시점 조회 시작: "+ now_time);
 		nowAvailableRoomList = service.nowAvailableRoom(now_time);
-		
-		for (StudyRoomVO vo : nowAvailableRoomList) {
-			System.out.println("현시점 조회가능리스트: " + vo);
-		}
-		
 		return nowAvailableRoomList;
 	}
 	
