284d447efac4e04311d2e5c50cea05f1a9b308f2
ulala8725 <30626463+ulala8725@users.noreply.github.com>
2017-09-28 19:02:03 +0900

    Update AdminDAO.java
    캘린더 관련부분 추가 20170928

--- Management/java/com/spring/manage/dao/AdminDAO.java
+++ Management/java/com/spring/manage/dao/AdminDAO.java
@@ -12,4 +12,15 @@ public interface AdminDAO {
 	public int returnBook(int num);
 	public int updateDelayed();
 	public int returnDelayedBook(int num);
+	
+	// 캘린더: 도서대여승인시 이벤트 테이블 상태변경 (대여요청->대여중, 요청일자->반납예정일자)
+	public void modifyLendEvent(Map<String, Integer> map);
+	// 캘린더: 도서대여승인 반려시 이벤트 테이블에서 삭제
+	public void deleteLendEvent(int num);
+	// 캘린더: 도서반납시 이벤트 테이블에서 상태 변경 (대여중->반납완료, 반납예정일자->반납완료일자)
+	public void endLendEvent(int num);
+	// 도서 연체 리스트 가져오기
+	public ArrayList<LendVO> getDelayedList();
+	// 캘린더: 도서연체시 이벤트 테이블에서 상태 변경(대여중->연체중, 반납예정일자->오늘)
+	public void delayedLendEvent(LendVO delayedList);
 }
